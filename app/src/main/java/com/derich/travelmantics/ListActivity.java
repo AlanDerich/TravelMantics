8b0332e0c91edae7e8a6cc831ab4ddeda765b55f
jamesnyakush <emvesttech@gmail.com>
2019-10-30 13:06:34 +0300

    debugged

--- app/src/main/java/com/derich/travelmantics/ListActivity.java
+++ app/src/main/java/com/derich/travelmantics/ListActivity.java
@@ -1,40 +1,28 @@
 package com.derich.travelmantics;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-
 public class ListActivity extends AppCompatActivity {
     RecyclerView rvDeals;
-    final DealAdapter deal = new DealAdapter();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-         rvDeals = findViewById(R.id.tvDeals);
 
-        rvDeals.setAdapter(deal);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false);
-        rvDeals.setLayoutManager(linearLayoutManager);
+        rvDeals = findViewById(R.id.tvDeals);
+        final DealAdapter adapter = new DealAdapter();
+        rvDeals.setAdapter(adapter);
+        rvDeals.setHasFixedSize(true);
+        rvDeals.setLayoutManager(new LinearLayoutManager(this));
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+
+
 }
